3ed010937427d8f0ddb844b9e0ead254208357d3
Aman Gupta <amangupta954055@gmail.com>
2024-03-04 12:01:12 +0530

    start development

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 
 export default function Home() {
+  const gsss = () => {
+    console.log("Hello Dev")
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main  className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
+
+
+
+
+
+
     </main>
   );
 }
