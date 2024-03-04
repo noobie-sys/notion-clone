8e457bdd856572c6304d2630d7092ad656d6dd5d
Aman Gupta <amangupta954055@gmail.com>
2024-03-04 12:22:55 +0530

    work on marketing page

---
+++ app/(marketing)/_components/Hero.tsx/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col items-center justify-center max-w-5xl'>
+        
+    </div>
+  )
+}
+
+export default Hero
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,17 @@
+
+import Image from "next/image";
+import Heading from "./_components/Heading";
+import Hero from "./_components/Hero.tsx/Hero";
+
+const MarketingPage = () => {
+  return (
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Hero />
+      </div>
+    </div>
+  );
+};
+
+export default MarketingPage;
