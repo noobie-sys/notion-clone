9b791385be1aab42ab6aad619fbfda50a37e3995
Aman Gupta <amangupta954055@gmail.com>
2024-03-04 14:07:15 +0530

    add clerk authentication and convex database

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,26 +3,27 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jotion",
   description: "The connected workspace where better, faster work happens",
-  icons : {
-    icon : [
+  icons: {
+    icon: [
       {
-        media : "(prefers-color-scheme : light)",
-        url : "/assets/logo.svg",
-        href : "/assets/logo.svg"
+        media: "(prefers-color-scheme : light)",
+        url: "/assets/logo.svg",
+        href: "/assets/logo.svg",
       },
       {
-        media : "(prefers-color-scheme : dark)",
-        url : "/assets/logo-dark.svg",
-        href : "/assets/logo-dark.svg"
-      }
-    ]
-  }
+        media: "(prefers-color-scheme : dark)",
+        url: "/assets/logo-dark.svg",
+        href: "/assets/logo-dark.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -33,15 +34,18 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-        storageKey="jotion-theme"
-        >
-        {children}
-        </ThemeProvider>
+        {/* <COnvexcle */}
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="jotion-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
